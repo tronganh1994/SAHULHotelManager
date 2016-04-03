423eeac255f65cd6de409c219b45c5e33c2e861d
Limited7247 <lt-dat@outlook.com>
2016-04-03 12:07:38 +0700

    Update Class
    Added new classes.

---
+++ SAHULHotelManager/DTO/CPerson.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAHULHotelManager.DTO
+{
+    class CPerson
+    {
+        #region "Atris"
+        private string Name;
+        private int Age;
+        #endregion
+
+        #region "Property"
+        public new string ToString
+        {
+            get
+            {
+                return Name + " " + Age;
+            }
+        }
+        #endregion
+
+        #region "Constructor"
+        public CPerson()
+        {
+
+        }
+        public CPerson(string Name, int Age)
+        {
+
+        }
+        #endregion
+
+        #region "Method"
+        public void Update(int Age)
+        {
+
+        }
+        #endregion
+    }
+}
