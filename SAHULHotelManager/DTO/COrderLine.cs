18cb3d736878e62fb09da2954bf72fb429e6ee83
Limited7247 <lt-dat@outlook.com>
2016-04-05 00:28:27 +0700

    Added COrderLine, ClassDiagram
    Added COrderLine, ClassDiagram

--- SAHULHotelManager/DTO/COrderLine.cs
+++ SAHULHotelManager/DTO/COrderLine.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace SAHULHotelManager.DTO
 {
-    class COrderLine
+    public class COrderLine
     {
+        private int id;
+        private int orderId;
+        private string title;
+        private double price;
+
+        public COrderLine()
+        {
+            this.id = 0;
+            this.orderId = 0;
+            this.title = "Unknown";
+            this.price = 0;
+        }
+        public COrderLine(int id, int orderId, string title, double price)
+        {
+            this.id = id;
+            this.orderId = orderId;
+            this.title = title;
+            this.price = price;
+        }
+        public string ShowInfors()
+        {
+            return id + "\t" + orderId + "\t" + title + "\t" + price;
+        }
     }
 }
