801d843e9ab7dfbcad657a51da942606fa61db6c
toilaanh <toilaanh23101996@gmail.com>
2016-04-04 23:32:26 +0700

    ADD class
    em tuấn anh em nộp Position và cho tùng class roomtype

--- SAHULHotelManager/DTO/CPosition.cs
+++ SAHULHotelManager/DTO/CPosition.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace SAHULHotelManager.DTO
 {
-    class CPosition
+    public class CPosition
     {
+        #region "attribus"
+        private int ID;
+        private string name;
+        private string departmentid;
+        #endregion
+        #region "construction"
+        //khoi tao khong tham so
+        public CPosition()
+        {
+            this.ID = 0;
+            this.name = string.Copy("");
+            this.departmentid = string.Copy("");
+        }
+        //khoi tao co tham so
+        public CPosition(int ID1, string name1, string department1)
+        {
+            this.ID = ID1;
+            this.name = string.Copy(name1);
+            this.departmentid = string.Copy(department1);
+        }
+        #endregion
+        #region "method"
+        //show doi tuong
+        public string ShowInfors()
+        {
+            return "\n\tID:" + ID + "\n\tNAME: " + name + "\n\tDEPARTMENT ID:" + departmentid;
+        }
+        #endregion
     }
 }
--- SAHULHotelManager/DTO/CRoomType.cs
+++ SAHULHotelManager/DTO/CRoomType.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace SAHULHotelManager.DTO
 {
-    class CRoomType
+    public class CRoomtype
     {
+        #region "thuoc tinh"
+        private string name;
+        private string area;
+        private string description;
+        private int id;
+        private int price;
+        #endregion
+        #region "khoi tao"
+        public CRoomtype()
+        {
+            this.name = string.Copy("");
+            this.area = string.Copy("");
+            this.description = string.Copy("");
+            this.id = 0;
+            this.price = 0;
+        }
+        public CRoomtype(int id1, string name1, string area1, string description1, int price1)
+        {
+            this.id = id1;
+            this.name = string.Copy(name1);
+            this.area = string.Copy(area1);
+            this.description = string.Copy(description1);
+            this.price = price1;
+        }
+        #endregion
+        #region "phuong thuc"
+        public string showroomtype()
+        {
+            return "\n                       chi tiet Roomtype              " + "\nid: " + id + "\nname: " + name + "\narea: " + area + "\ndescription: " + description + "\nprice: {0}" + price;
+        }
+        #endregion
     }
 }
